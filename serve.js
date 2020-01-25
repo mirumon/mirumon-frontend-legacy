107f967f288f4baede3e1e99f2fe2e5e5005a7df
Nick Khitrov <khitrov34@gmail.com>
2020-01-25 14:52:55 +0300

    Add deploy github action (#8)
    * Change port from 80 to 8000
    
    * Add deploy action

--- serve.js
+++ serve.js
@@ -3,5 +3,5 @@ const express = require('express');
 const app = express();
 const path = require('path');
 app.use(express.static(path.join(__dirname, '/build')));
-app.listen(80);
-console.log('Running on http://localhost:80');
+app.listen(8000);
+console.log('Running on http://localhost:8000');
