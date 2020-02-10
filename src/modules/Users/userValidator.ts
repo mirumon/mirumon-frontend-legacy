cb822ea0bd01974aa6829a2b6126d9aaca77a0d8
Wellini <i4iksy@yahoo.com>
2020-02-10 11:44:12 +0300

    Add validator rules

--- src/modules/Users/userValidator.ts
+++ src/modules/Users/userValidator.ts
@@ -8,9 +8,24 @@ const userValidator: (result: Record<string, TableType>) => ValidatorError = (re
     if((result.password as string).length < 8)
         errors.push({
             belongsTo: 'password',
-            message: 'Password must contain at least 8 symbols!'
+            message: 'At least 8 symbols!'
         })
-    else if(result.password !== result.confirm_password)
+    if((result.password as string).search(/[0-9]/) === -1) 
+        errors.push({
+            belongsTo: 'password',
+            message: 'At least one digit!'
+        })
+    if((result.password as string).search(/[A-Z]/) === -1) 
+        errors.push({
+            belongsTo: 'password',
+            message: 'At least one capital letter!'
+        })
+    if((result.password as string).search(/[a-z]/) === -1) 
+        errors.push({
+            belongsTo: 'password',
+            message: 'At least one lowercase letter!'
+        })
+    if(result.password !== result.confirm_password)
         errors.push({
             belongsTo: 'password',
             message: 'Passwords must match!'
